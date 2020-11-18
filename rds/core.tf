9d6e5808cf70466fe21dd15db042745e5ec1c6c0
Tiago Borges <tiago@borges.vip>
2020-11-18 10:50:56 +0000

    SG and RDS modules

--- rds/core.tf
+++ rds/core.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "this" {
 }
 
 module "sgs" {
-  source       = "../security_group"
+  source = "git::ssh://git@gitlab.com/enxcs/workload/ecs-shared/terraform-modules/vpc.git//security_group?ref=v1.0.2"
   project_name = var.project_name
   description  = "${var.project_name} RDS security group"
   vpc_id       = var.vpc_id
--- vpc/output.tf
+++ vpc/output.tf
@@ -7,11 +7,11 @@ output "edge_subnets" {
 }
 
 output "dmz_subnets" {
-  value = aws_subnet.edge.*.id
+  value = aws_subnet.dmz.*.id
 }
 
 output "data_subnets" {
-  value = aws_subnet.edge.*.id
+  value = aws_subnet.data.*.id
 }
 
 output "vpc_id" {
