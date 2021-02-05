edeecbf6cfa26dea9488ac1ca2f164d83847b33b
Tiago Borges <tiago@borges.vip>
2021-02-05 14:34:42 +0000

    Revert "Revert "refactor eks modules""
    This reverts commit 57239e8659340e398c8a5e4d694e5953ef4f3bc9.

--- eks/cluster/output.tf
+++ eks/cluster/output.tf
@@ -10,9 +10,9 @@ output "cluster_certificate" {
   value = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
 }
 
-# output "eks_worker_role" {
-#   value = aws_iam_role.eks_worker.arn
-# }
+output "eks_worker_role" {
+  value = aws_iam_role.eks_worker.arn
+}
 
 # output "eks_cluster_admin_role" {
 #   value = aws_iam_role.cluster-admin.name
