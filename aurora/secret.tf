f2850c1ae8bcbfcc4c4a0ec96245ac547ecc43e4
Tiago Borges <tiago@borges.vip>
2021-02-12 07:04:08 +0000

    Aurora module

---
+++ aurora/secret.tf
@@ -0,0 +1,13 @@
+resource "random_password" "random_string" {
+  length  = 20
+  special = false
+}
+
+resource "aws_secretsmanager_secret" "secret" {
+  name = "${var.project_name}/db/credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "secret_val" {
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = "{\"DB_NAME\":\"${var.db_name}\", \"DB_USER\":\"${var.db_user}\",\"DB_PASSWORD\":\"${random_password.random_string.result}\",\"DB_HOST\":\"${aws_rds_cluster_instance.this.endpoint}\",\"DB_PORT\":\"${aws_rds_cluster_instance.this.port}\"}"
+}
