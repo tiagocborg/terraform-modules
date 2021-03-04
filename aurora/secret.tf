1801ef244d5d4012870b83a46379a70335cbd353
Tiago Borges <tiago@borges.vip>
2021-03-04 13:37:22 +0000

    Fix rds secret

--- aurora/secret.tf
+++ aurora/secret.tf
@@ -4,5 +4,5 @@ resource "aws_secretsmanager_secret" "secret" {
 
 resource "aws_secretsmanager_secret_version" "secret_val" {
   secret_id     = aws_secretsmanager_secret.secret.id
-  secret_string = "{\"DB_NAME\":\"${var.db_name}\", \"DB_USER\":\"${var.db_user}\",\"DB_PASSWORD\":\"${random_password.master_password.0.result}\",\"DB_HOST\":\"${aws_rds_cluster_instance.this.0.endpoint}\",\"DB_PORT\":\"${aws_rds_cluster_instance.this.0.port}\"}"
+  secret_string = "{\"DB_NAME\":\"${var.database_name}\", \"DB_USER\":\"${var.username}\",\"DB_PASSWORD\":\"${random_password.master_password.0.result}\",\"DB_HOST\":\"${aws_rds_cluster_instance.this.0.endpoint}\",\"DB_PORT\":\"${aws_rds_cluster_instance.this.0.port}\"}"
 }
